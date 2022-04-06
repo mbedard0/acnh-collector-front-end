0e1292aa886316dc726dbc869c6d0344504fae88
Meiling Bedard <meiling.bedard@gmail.com>
2022-04-06 14:39:24 -0600

    set up clothing page

--- src/pages/Clothing/Clothing.jsx
+++ src/pages/Clothing/Clothing.jsx
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import * as clothingService from '../../services/clothingService'
+
 const Clothing = (props) => {
-  return ( 
-  <>
-  </>
+  const [clothing, setClothing] = useState([])
+
+  useEffect(() => {
+    async function getClothing() {
+      const allClothing = await clothingService.getAllClothing()
+      setClothing(allClothing)
+    }
+    getClothing()
+  }, [])
+
+
+  return (
+    <>
+    {clothing.map(clothes => {
+      return(
+        <div>
+        {clothes.name}
+        </div>
+      )
+    })}
+    </>
   );
 }
 
