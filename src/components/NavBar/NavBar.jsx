15f5877996345ed542185a1c60a200a3c8ba21fc
Meiling Bedard <meiling.bedard@gmail.com>
2022-04-07 22:27:05 -0600

    add fossils to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,7 +22,15 @@ const NavBar = ({ user, handleLogout }) => {
                   <li><Link to='/sea-creatures'>Sea Creatures</Link></li>
                 </ul>
               </li>
-              <li><Link to='/museum-artifacts'>Museum Artifacts</Link></li>
+              <li tabIndex="0" className="dropdown dropdown-end">
+                <a className="">
+                  Museum Artifacts
+                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                </a>
+                <ul class="p-2 shadow bg-base-100 rounded-box dropdown-content">
+                  <li><Link to="/fossils">Fossils</Link></li>
+                </ul>
+              </li>
               <li><Link to='/furniture'>Furniture</Link></li>
               <li><Link to='clothing'>Clothing</Link></li>
               <li tabIndex="0" className="dropdown dropdown-end">
